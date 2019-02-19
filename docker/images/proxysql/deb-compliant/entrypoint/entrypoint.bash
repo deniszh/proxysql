d571cde09f973aa4de62945100480b9cb0f98676
deniszh <denis.zhdanov@gmail.com>
2019-02-19 19:59:05 +0100

    Some fixes

--- docker/images/proxysql/deb-compliant/entrypoint/entrypoint.bash
+++ docker/images/proxysql/deb-compliant/entrypoint/entrypoint.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Delete package if exists
 rm -f /opt/proxysql/binaries/proxysql_${CURVER}-${PKG_RELEASE}_amd64.deb || true && \
+rm -f percona-release_0.1-4.precise_all.deb || true && \
 # Cleanup relic directories from a previously failed build
 rm -f /opt/proxysql/proxysql.ctl || true && \
 # Clean and build dependancies and source
@@ -18,7 +19,7 @@ if [ "`grep Ubuntu /etc/issue | awk '{print $2}' | cut -d. -f1`" == "12" ]; then
     dpkg -i percona-release_0.1-4.precise_all.deb
     apt-get update
     apt-get install -y cmake gcc-4.8 g++-4.8 libboost-all-dev libperconaserverclient20=5.7.18-16-1.precise libperconaserverclient20-dev=5.7.18-16-1.precise percona-server-source-5.7=5.7.18-16-1.precise
-    apt-get -y autoremove && apt-get -y remove libmysqlclient-dev ibmysqlclient18 libmysqlclient18-dev libmysqlclient18.1 libmysqlclient18.1-de
+    apt-get -y autoremove && apt-get -y remove libmysqlclient-dev ibmysqlclient18 libmysqlclient18-dev libmysqlclient18.1 libmysqlclient18.1-dev
     update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
     cd /opt/proxysql/deps/re2/
@@ -28,19 +29,21 @@ if [ "`grep Ubuntu /etc/issue | awk '{print $2}' | cut -d. -f1`" == "12" ]; then
 fi
 # cloning binlog server
 cd /opt && git clone https://github.com/sysown/proxysql_mysqlbinlog.git && cd /opt/proxysql_mysqlbinlog && \
-# building libslave
+# copying hash.h from Percona server sources
 cd /usr/src/percona-server/ && tar -xpzf percona-server-5.7_5.7.18-16.orig.tar.gz && cp -fv /usr/src/percona-server/percona-server-5.7.18-16/include/hash.h /usr/include/mysql/ && \
+# mimicking Percona libs as Mysql libs (because no MySQL 5.7 dev exist for precise)
 cd /usr/lib/x86_64-linux-gnu/ && ln -s libperconaserverclient.a libmysqlclient.a && ln -s libperconaserverclient.so libmysqlclient.so && \
+# building libslave (only slave_a lib, tests are failing)
 cd /opt/proxysql_mysqlbinlog/libslave/ && cmake . && make slave_a && \
 # building binlog server
 cd ../ && make && \
-mkdir -p /opt/proxysql/bin && \
-cp /opt/proxysql_mysqlbinlog/proxysql_binlog_reader /opt/proxysql/bin/proxysql_binlog_reader && \
+cp /opt/proxysql_mysqlbinlog/proxysql_binlog_reader /opt/proxysql/ && \
 # build proxysql
 cd /opt/proxysql && \
 ${MAKE} cleanbuild && \
 ${MAKE} ${MAKEOPT} build_deps && \
 ${MAKE} ${MAKEOPT} && \
+ls -al &&
 # Prepare package files and build package
 cp /root/ctl/proxysql.ctl /opt/proxysql/proxysql.ctl && \
 sed -i "s/PKG_VERSION_CURVER/${CURVER}/g" /opt/proxysql/proxysql.ctl && \
